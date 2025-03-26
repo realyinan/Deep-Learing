26df0fb5ffce139dc10892fabe941c22a05abf77
Paul Harrison <19981014liu@gmail.com>
2025-03-26 22:59:17 +0800

    20250326

--- 01_Pytorch的基本使用/14-pytorch构建线性回归模型.py
+++ 01_Pytorch的基本使用/14-pytorch构建线性回归模型.py
@@ -21,8 +21,8 @@ def create_datasets():
         random_state=0
     )
 
-    x = torch.tensor(data=x)
-    y = torch.tensor(data=y)
+    x = torch.tensor(data=x, dtype=torch.float32)
+    y = torch.tensor(data=y, dtype=torch.float32)
 
     return x, y, coef
 
@@ -31,11 +31,63 @@ def create_datasets():
 def train(x, y, coef):
     dataset = TensorDataset(x, y)  # 创建张量数据集对象
     dataloader = DataLoader(dataset=dataset, batch_size=16, shuffle=True)  # 创建数据加载集对象
+
     # in_features: 每个输入样本的特征维度是多少
     # out_features: 输出结果希望变成几维（映射到哪个维度）
     model = nn.Linear(in_features=1, out_features=1)
+    # print(model.weight)
+    # print(model.bias)
+    # print(list(model.parameters()))
+    # print(model)
     criterion = nn.MSELoss()
+    # 创建SDG优化器, 更新w和b
+    optimizer = SGD(params=model.parameters(), lr=0.01)
 
+    epochs = 100  # 训练次数
+    losses = []  # 储存每次训练的平均损失值
+    total_loss = 0.0
+    train_samples = 0
+
+    for epoch in range(epochs):
+        for train_x, train_y in dataloader:
+            # train_x -> float64
+            # w -> float32
+            y_pred = model(train_x)
+            # y_pred: 二维张量
+            # train_y: 一维张量, 需要修改为二维张量, n行1列
+            loss = criterion(y_pred, train_y.reshape(-1, 1))
+            # 获取loss张量的item()
+            total_loss += loss.item()
+            train_samples += 1
+            # 梯度清零
+            optimizer.zero_grad()
+            # 计算梯度值
+            loss.backward()
+            # 梯度更新w和b
+            optimizer.step()
+        losses.append(total_loss / train_samples)
+        print(f"每次训练的平均损失值: {total_loss / train_samples}")
+    print(f"loss列表: {losses}")
+    print(f"w: {model.weight}")
+    print(f"b: {model.bias}")
+
+    # 绘制每次训练损失值曲线变化图
+    plt.plot(range(epochs), losses)
+    plt.title("损失值曲线变化图")
+    plt.grid()
+    plt.show()
+
+    # 绘制预测图和真实值的对比图
+    plt.scatter(x, y)
+    y1 = torch.tensor(data=[model.weight*v + model.bias for v in x])  # 预测值
+    y2 = torch.tensor(data=[v * coef + 14.5 for v in x])
+    plt.plot(x, y1, label="训练")
+    plt.plot(x, y2, label="真实")
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+     
 if __name__ == "__main__":
     x, y, coef = create_datasets()
     train(x, y, coef)
---
+++ 02_神经网络基础/06-搭建神经网络模型.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+from torchsummary import summary
+
+
+class ModelDemo(nn.Module):
+    # 初始化属性
+    def __init__(self):
+        super().__init__()  # 调用父类的初始化属性
+        # 创建第一个隐藏层模型
+        self.linear1 = nn.Linear(in_features=3, out_features=3)
+        nn.init.xavier_normal_(self.linear1.weight)
+        nn.init.zeros_(self.linear1.bias)
+
+        # 创建第二个隐藏层模型
+        self.linear2 = nn.Linear(in_features=3, out_features=2)
+        nn.init.kaiming_normal_(self.linear2.weight, nonlinearity='relu')
+        nn.init.zeros_(self.linear2.bias)
+
+        # 创建第三个隐藏层模型
+        self.output = nn.Linear(in_features=2, out_features=2)
+
+    # 前向传播方法, 会自动调用
+    def forward(self, x):
+        x = self.linear1(x)
+        x = torch.sigmoid(x)
+        x = self.linear2(x)
+        x = torch.relu(x)
+        x = self.output(x)
+        x = torch.softmax(input=x, dim=1)
+        return x
+    
+def train():
+    my_model = ModelDemo()
+    # 构造数据集样本, 随机生成
+    data = torch.randn(size=(5, 3))  # 会自动调用forward()函数
+    print(f"data: {data}")
+    print(f"data.shape: {data.shape}")
+
+    # 调用神经网络模型对象进行训练
+    output = my_model(data)
+    print(f"output: {output}")
+    print(f"output.shape: {output.shape}")
+
+    print("=======================计算和查看模型参数==============================")
+    summary(model=my_model, input_size=(5, 3))
+    for name, param in my_model.named_parameters():
+        print(f"name: {name}")
+        print(f"param: {param}")
+
+
+if __name__ == "__main__":
+    train()
+
